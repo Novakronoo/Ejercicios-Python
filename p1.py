6fd84ae7c1904358f1862784ec1a3dd94a477960
Lucas Clavijo <lucas15redes@gmail.com>
2026-09-19 09:55:30 -0300

    Agregue ejercicios teóricos para realizar del 6 al 12 con dificultad media

--- p1.py
+++ p1.py
@@ -1,18 +1,18 @@
-#1. Calculadora de IMC
-#Pedí (con input()) peso en kg y altura en metros, calculá el IMC (peso / altura**2)
-# e imprimí un mensaje según el resultado: "bajo peso" (<18.5), "normal" (18.5-24.9), "sobrepeso" (>25).
+#Problema 1 — Cruzar la calle de forma segura
+#Describí, paso a paso, el proceso completo para cruzar una calle con semáforo de forma segura,
+#desde que llegás a la esquina hasta que estás del otro lado.
+#   Variables: Persona, Semaforo
+#   Funciones; Cruzar_calle
 
-p = float(input("Ingrese su peso: "))
-a = float(input("Ingrese su altura: "))
+persona = input("Ingrese su nombre: ")
+semaforo = input("Ingrese el color del semáforo(rojo, amarillo, verde) :")
 
-imc = p / a ** 2
+def cruzar_calle(semaforo):
+    if semaforo == "verde":
+        return f"{persona} puede cruzar la calle de forma segura."
+    elif semaforo == "amarillo":
+        return f"{persona} debe esperar, el semáforo está en amarillo."
+    elif semaforo == "rojo":
+        return f"{persona} debe esperar, el semáforo está en rojo."
 
-def resultado(imc):
-    if imc < 18.5:
-        return "bajo peso"
-    elif 18.5 <= imc <= 24.9:
-        return "normal"
-    else:
-        return "sobrepeso"
-
-print("Su resultado es: ", resultado(imc))
+print(cruzar_calle(semaforo))
